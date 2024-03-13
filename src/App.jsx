e621497c27f105283d4e2a75347462c3cf1dbf45
Eliel <elielsf13@gmail.com>
2024-03-12 21:07:28 -0300

    Adicionar principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import styles from './App.module.css'
+import style from './App.module.css';
 
 export default function App() {
   return (
-    <div>
-
+    <div className={style.container}>
+        <header className={style.header}>
+          <h1>a</h1>
+        </header>
+        <main className={style.main}>
+          <h1>b</h1>
+        </main>
+        <footer className={style.footer}>
+          <h1>b</h1>
+        </footer>
     </div>
   )
 }
